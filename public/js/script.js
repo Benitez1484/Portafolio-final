2b85b6ef944007b1549da149fc7a5952ccb6a86e
Benitez1484 <santosbenitez2017@gmail.com>
2024-10-29 19:05:56 -0600

    Actualización en script.js

--- public/js/script.js
+++ public/js/script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     menuToggle.addEventListener('click', function() {
       navMenu.classList.toggle('active');
     });
+    
 
     // Cerrar menú al hacer clic en un enlace
     const navLinks = navMenu.querySelectorAll('a');
